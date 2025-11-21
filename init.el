0bebf5df26539abbd1f371a8213d6ff4a999046e
confusedalex <alex@confusedalex.dev>
2025-11-21 09:54:58 +0100

    refactor: cleanup

--- init.el
+++ init.el
@@ -711,8 +711,6 @@
   :after dart-mode
   :bind (:map dart-mode-map
               ("C-M-x" . #'flutter-run-or-hot-reload))
-  ;; :custom
-  ;; (flutter-sdk-path "/Applications/flutter/")
   )
 
 (use-package kdl-mode
@@ -813,15 +811,3 @@
   :config
   (require 'evil-org-agenda)
   (evil-org-agenda-set-keys))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
