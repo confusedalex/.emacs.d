dfd69c24ae3bb2f8c3968035729a08e15936cf23
confusedalex <alex@confusedalex.dev>
2026-03-20 10:30:24 +0100

    feat: add copilot

--- init.el
+++ init.el
@@ -850,4 +850,14 @@
   (require 'evil-org-agenda)
   (evil-org-agenda-set-keys))
 
+(use-package copilot
+  :hook (prog-mode . copilot-mode)
+  :bind (:map copilot-completion-map
+              ("<tab>" . copilot-accept-completion)
+              ("TAB" . copilot-accept-completion)
+              ("C-<tab>" . copilot-accept-completion-by-word)
+              ("C-TAB" . copilot-accept-completion-by-word)
+              ("C-n" . copilot-next-completion)
+              ("C-p" . copilot-previous-completion)))
+
 ;;; init.el ends here
