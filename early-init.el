0150352d160454c5901e9ebec324fef82c850194
confusedalex <alex@confusedalex.dev>
2026-08-09 11:36:02 +0200

    use early-init to mitage a package loading issue with straight

---
+++ early-init.el
@@ -0,0 +1,24 @@
+;; early-init
+;; This runs before normal startup and makes sure Org comes from straight.
+
+;; [[file:README.org::*early-init][early-init:1]]
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name
+        "straight/repos/straight.el/bootstrap.el"
+        (or (bound-and-true-p straight-base-dir)
+            user-emacs-directory)))
+      (bootstrap-version 7))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(straight-use-package 'org)
+(require 'org-macs)
+(require 'org)
+;; early-init:1 ends here
--- init.el
+++ init.el
@@ -6,24 +6,6 @@
 ;; currently I don't make use of both these features.
 
 ;; [[file:README.org::*Package Management][Package Management:1]]
-;; Bootstrap Straight.el
-(defvar bootstrap-version)
-(let ((bootstrap-file
-       (expand-file-name
-        "straight/repos/straight.el/bootstrap.el"
-        (or (bound-and-true-p straight-base-dir)
-            user-emacs-directory)))
-      (bootstrap-version 7))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
-
-;;; Package setup -----
 (straight-use-package 'use-package)
 
 (use-package straight
